12ad5b80ad67ccfe03d0946b1f888ee04c853c3b
Eugene Chechurin <e.chechurin@gmail.com>
2017-08-24 10:54:51 -0700

    cors-headers example

--- js/background.js
+++ js/background.js
@@ -1,3 +1,23 @@
-browser.browserAction.onClicked.addListener(function(tab) {
-            alert("Hello world");
-          });
+
+ if (browser) browser.webRequest.onHeadersReceived.addListener(
+    // extendHeadersWithContentSecurityPolicies,
+    function(details) {
+      var responseHeaders = details.responseHeaders;
+
+      if (responseHeaders) {
+        return {
+          responseHeaders: [
+            {
+              name: 'content-security-policy',
+              value: '*'
+            }
+          ]
+        };
+      }
+      return;
+    },
+    {
+      urls: ["<all_urls>"]
+    },
+    ["blocking", "responseHeaders"]
+  );
